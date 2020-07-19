dd31bc815c1b6edace26d969f23f71bd98f85196
Melkibson <melkiyamna@protonmail.com>
2020-07-19 15:28:01 +0200

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+import { render } from '@testing-library/react';
+
+const Header = styled.header`
+    width: 100%;
+    height: 200px;
+    `;
+
+render(
+    <Header></Header>
+)
